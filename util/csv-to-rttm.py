2e6779d9cec0262451d6e49c459c458ce19dc95c
Finn Innes <finn123890@gmail.com>
2023-04-04 13:51:04 +1200

    fixed conversion not working, added recordingID as optional parameter

--- util/csv-to-rttm.py
+++ util/csv-to-rttm.py
@@ -7,26 +7,31 @@ parser = argparse.ArgumentParser(
   formatter_class = argparse.ArgumentDefaultsHelpFormatter, # includes default values in help menu
 ) 
 
-parser.add_argument("inputfile", type=argparse.FileType("r"), help="Input file (.csv)")
-parser.add_argument("outputfile", type=argparse.FileType("w"), help="Output file (.rttm)")
+parser.add_argument("inputfile", help="Input file (.csv)")
+parser.add_argument("outputfile", help="Output file (.rttm)")
+parser.add_argument("-id", "--recordingid", help="ID of diarization data (e.g. audio name), gets stored in RTTM")
 args = parser.parse_args()
 
+inputFile = getattr(args, "inputfile")
+print(inputFile)
 fileText, fileExtension = os.path.splitext(getattr(args, "inputfile"))
 fileName = fileText + fileExtension
-outputFile = getattr(args, "outputfile"):
+outputFile = getattr(args, "outputfile")
+recordingID = getattr(args, "recordingid")
 
 try:
   if "csv" not in fileExtension.lower():
     sys.exit("ERROR: Input file: " + fileName + " is not a CSV")
   if "rttm" not in outputFile.lower():
     sys.exit("ERROR: Output file: " + outputFile + " is not a RTTM")
-    
+  if recordingID is None:  
+    recordingID = fileName
   print("starting conversion of input file '" + fileName + "' to RTTM...")
   with open(outputFile, mode="w") as rttm_writer:
     with open(fileName, "r") as csv_input:
       reader = csv.reader(csv_input, delimiter=",")
       for i, line in enumerate(reader):
-        rttm_writer.write(" ".join(["SPEAKER", fileText, "1", line[1], str(Decimal(line[2])-Decimal(line[1])), "<NA>", "<NA>", line[0], "<NA>", "<NA>"]) + "\n")
+        rttm_writer.write(" ".join(["SPEAKER", recordingID, "1", line[1], str(Decimal(line[2])-Decimal(line[1])), "<NA>", "<NA>", line[0], "<NA>", "<NA>"]) + "\n")
   print("conversion finished with output file " + outputFile)
 except Exception as e: 
   print(e)
